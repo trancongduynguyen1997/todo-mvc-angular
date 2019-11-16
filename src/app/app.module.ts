143e91a1c09f7d6659037fabe1786a5a896028b8
TranCongDuyNguyen <tcdnguyen1997@gmail.com>
2019-11-16 09:05:48 +0700

    start learning Angularrr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoInputComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/model/filter-button.model.ts
@@ -0,0 +1,9 @@
+export interface FilterButton {
+  type: Filter;
+  label: string;
+  isActived: boolean;
+}
+
+export enum Filter {
+  All, Active, Completed
+}
