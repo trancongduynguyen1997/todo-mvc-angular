001b1bab9c66754fd252aa138c5b548598d155e9
TranCongDuyNguyen <tcdnguyen1997@gmail.com>
2019-11-17 10:25:11 +0700

    style header, input vs footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterButton, Filter } from '../model/filter-button.model';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  filterButtons: FilterButton[] = [
+    {type: Filter.All, label: 'All', isActived: true},
+    {type: Filter.Active, label: 'Active', isActived: false},
+    {type: Filter.Completed, label: 'Completed', isActived: false}
+  ];
 
+  length = 0;
   constructor() { }
 
   ngOnInit() {
